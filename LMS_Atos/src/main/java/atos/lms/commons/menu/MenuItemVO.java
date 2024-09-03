e052cecd7f0d9bd8133420f867621b7f49d507ea
SeokJune <op2se1@gmail.com>
2024-09-03 14:24:47 +0900

    Create Menu Files
    1. 메뉴 VO
    2. 메뉴 작업 Class
    3. 메뉴관련 message props

---
+++ LMS_Atos/src/main/java/atos/lms/commons/menu/MenuItemVO.java
@@ -0,0 +1,58 @@
+package atos.lms.commons.menu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuItemVO {
+	private String title;
+	private String url;
+	private String data;
+	private List<MenuItemVO> subMenuItems;
+
+	public MenuItemVO() {
+		this.subMenuItems = new ArrayList<>();
+	}
+
+	public MenuItemVO(String title, String url, String data) {
+		this.title = title;
+		this.url = url;
+		this.data = data;
+		this.subMenuItems = new ArrayList<>();
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public List<MenuItemVO> getSubMenuItems() {
+		return subMenuItems;
+	}
+
+	public void setSubMenuItems(List<MenuItemVO> subMenuItems) {
+		this.subMenuItems = subMenuItems;
+	}
+
+	public void addSubMenuItem(MenuItemVO subMenuItem) {
+		this.subMenuItems.add(subMenuItem);
+	}
+}
